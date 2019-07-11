dec0c3b90e648a459ea33b63bf8b952c8776357f
Sagar Lohana <sagar_lohana@hotmail.com>
2019-07-11 00:08:12 -0400

    Removed unnecessary files

--- src/script.py
+++ src/script.py
@@ -2,9 +2,6 @@ import requests
 import re
 from bs4 import BeautifulSoup
 
-# url = input("Enter a URL: ")
-
-# url = "https://www.amazon.ca/dp/B0749WPNHM/ref=ods_gw_ha_H1_d_rr_dinner_tall_ca_061619_en?pf_rd_p=633d60bd-7787-4eb3-88fa-a07caff66213&pf_rd_r=NMCP7WHB6PFJ74B4TVXK"
 url = "https://www.amazon.ca/Anker-Headphones-Lightweight-Connection-Sweatproof/dp/B01N6DC2ZE/ref=sr_1_1?keywords=bluetooth+headphones&qid=1562557096&s=gateway&sr=8-1"
 desired_price = 40.00
 
@@ -30,13 +27,3 @@ if(desired_price > float(result)):
     print("Yes you can purchase it")
 else:
     print("No not in your price range")
-
-# output = str(soup.prettify)
-# print(output)
-# file = open('sample.txt', 'w')
-# file.write(output)
-# file.close()
-
-# print (soup.prettify)
-# price = soup.find("span", id="priceblock_ourprice")
-# print("The price is {0}".format(price.text))
