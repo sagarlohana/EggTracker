2773ffeb3199b3f04518405ea8a2e3aea3b9c30e
Sagar Lohana <sagar_lohana@hotmail.com>
2019-09-08 10:36:31 -0400

    Change scraping requests to async

--- src/script.py
+++ src/script.py
@@ -1,37 +1,16 @@
+import grequests
 import requests
 import re
 from bs4 import BeautifulSoup
 
 def track(url_list, thumbnail_lst, actual_prices_lst, can_buy):
-# url = "https://www.amazon.ca/Anker-Headphones-Lightweight-Connection-Sweatproof/dp/B01N6DC2ZE/ref=sr_1_1?keywords=bluetooth+headphones&qid=1562557096&s=gateway&sr=8-1"
-# desired_price = 40.00
 
-# print("You just entered this url: {0}".format(url))
-# try:
-#     result = requests.get(url)
-# except:
-#     print("You have provided an incorrect URL, please re-run the script providing a valid URL (perhaps prepend https://)")
-#     exit()
+    # Asynchronous response retrieval using grequests
+    reqs = [grequests.get(url.url) for url in url_list]
+    responses = grequests.map(reqs)
 
-# data = result.text
-# soup = BeautifulSoup(data, 'html.parser')
-
-# spans = soup.find('span', {'id' : 'priceblock_ourprice'}) # Extracts span containing price from Soup object
-# price = spans.get_text() # Extracts price out of span
-# result = re.sub(r"[^\d\.]", "", price) # Uses regex to extract price out of string
-
-# print(price)
-# print(result)
-
-# # Pseudo structure for how checking for price feasibility will work
-# if(desired_price >= float(result)):
-#     print("Yes you can purchase it")
-# else:
-#     print("No not in your price range")
-    for url in url_list:  
-        result = requests.get(url.url)
-
-        soup = BeautifulSoup(result.text, 'html.parser')
+    for i in range(len(responses)):
+        soup = BeautifulSoup(responses[i].text, 'html.parser')
 
         # Outputs the result of the soup in out.txt
         # with open('out.txt', 'w') as f:
@@ -53,7 +32,7 @@ def track(url_list, thumbnail_lst, actual_prices_lst, can_buy):
         thumbnail_lst += [thumbnail]
         
         # Add purchasability to list
-        if current_price <= url.price:
+        if current_price <= url_list[i].price:
             can_buy += [True]
         else:
             can_buy += [False]
